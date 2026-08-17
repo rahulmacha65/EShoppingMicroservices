ff5be51bf13cc2644b39675975b5d8579fda364d
Rahul Macha <rahul.macha567@gmail.com>
2026-08-17 11:44:55 +0530

    added update and delete product endpoints

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+
+public record DeleteProductResult(bool IsSuccess);
+public class DeleteProductHandler(IDocumentSession session, ILogger<DeleteProductCommand> logger) : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+{
+    public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Deleting product with Id: {Id}", command.Id);
+
+        session.Delete<Product>(command.Id);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new DeleteProductResult(true);
+    }
+}
+
---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,33 @@
+﻿namespace Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<UpdateProductResult>;
+public record UpdateProductResult(bool IsSuccess);
+public class UpdateProductHandler(IDocumentSession session, ILogger<UpdateProductHandler> logger) : IRequestHandler<UpdateProductCommand, UpdateProductResult>
+{
+    public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Handling UpdateProductCommand for product: {Name}", command.Name);
+
+        Product? existingProduct = await session.LoadAsync<Product>(command.Id, cancellationToken);
+        if (existingProduct == null)
+        {
+            logger.LogWarning("Product with name {Name} not found.", command.Name);
+            throw new ProductNotFoundException();
+        }
+        else
+        {
+            existingProduct.Name = command.Name;
+            existingProduct.Category = command.Category;
+            existingProduct.Description = command.Description;
+            existingProduct.ImageFile = command.ImageFile;
+            existingProduct.Price = command.Price;
+
+            session.Update(existingProduct);
+            await session.SaveChangesAsync(cancellationToken);
+
+            logger.LogInformation("Product with name {Name} updated successfully.", command.Name);
+            return new UpdateProductResult(true);
+        }
+    }
+}
+
