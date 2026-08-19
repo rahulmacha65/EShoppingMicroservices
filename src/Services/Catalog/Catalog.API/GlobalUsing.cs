daa54e0cdf3bfc96cd2395d52bd87466b1133503
Rahul Macha <rahul.macha567@gmail.com>
2026-08-19 19:58:58 +0530

    added seed data for development environment

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -2,6 +2,7 @@
 global using BuildingBlocks.CQRS;
 global using BuildingBlocks.Exceptions.Handler;
 global using Carter;
+global using Catalog.API.Data;
 global using Catalog.API.Exceptions;
 global using Catalog.API.Models;
 global using FluentValidation;
