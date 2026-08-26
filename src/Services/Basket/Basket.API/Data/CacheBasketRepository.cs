76d0583b20983d118c116d2dbc5a4577bbb527f1
Rahul Macha <rahul.macha567@gmail.com>
2026-08-26 19:35:16 +0530

    decorator pattern with redis

---
+++ src/Services/Basket/Basket.API/Data/CacheBasketRepository.cs
@@ -0,0 +1,41 @@
+﻿
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Data;
+
+public class CacheBasketRepository(IBasketRepository repository, IDistributedCache cache) : IBasketRepository
+{
+    public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        await repository.DeleteBasket(userName, cancellationToken);
+
+        await cache.RemoveAsync(userName, cancellationToken);
+
+        return true;
+    }
+
+    public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        var cachedBasket = await cache.GetStringAsync(userName, cancellationToken);
+        if (!string.IsNullOrEmpty(cachedBasket))
+        {
+            return JsonSerializer.Deserialize<ShoppingCart>(cachedBasket)!;
+        }
+        else
+        {
+            var basket = await repository.GetBasket(userName, cancellationToken);
+            await cache.SetStringAsync(userName, JsonSerializer.Serialize<ShoppingCart>(basket), cancellationToken);
+            return basket;
+        }
+    }
+
+    public async Task<ShoppingCart> StoreBasket(ShoppingCart cart, CancellationToken cancellationToken = default)
+    {
+        await repository.StoreBasket(cart, cancellationToken);
+
+        await cache.SetStringAsync(cart.UserName, JsonSerializer.Serialize(cart), cancellationToken);
+
+        return cart;
+    }
+}
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,8 +1,27 @@
+//using Microsoft.Extensions.Caching.Distributed;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the DI container.
 builder.Services.AddCarter();
+
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+//Decorating using Scrutor libaray
+builder.Services.Decorate<IBasketRepository, CacheBasketRepository>();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
+// manually decorating cachebasketrepository with distributed caching
+//builder.Services.AddScoped<IBasketRepository>(provider =>
+//{
+//    var basketRepository = provider.GetRequiredService<BasketRepository>();
+//    var distributedCache = provider.GetRequiredService<IDistributedCache>();
+//    return new CacheBasketRepository(basketRepository, distributedCache);
+//});
+
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 builder.Services.AddMediatR(ofg =>
 {
