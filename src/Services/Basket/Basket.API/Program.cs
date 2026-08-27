6fe6b64856cc45110ddc8b95d17e6811e1a481dc
Rahul Macha <rahul.macha567@gmail.com>
2026-08-27 17:30:19 +0530

    added health check with asp.net health check nuget

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,5 +1,8 @@
 //using Microsoft.Extensions.Caching.Distributed;
 
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the DI container.
@@ -23,6 +26,7 @@ builder.Services.AddStackExchangeRedisCache(options =>
 //});
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+builder.Services.AddHealthChecks();
 builder.Services.AddMediatR(ofg =>
 {
     ofg.RegisterServicesFromAssembly(typeof(Program).Assembly);
@@ -34,9 +38,18 @@ builder.Services.AddMarten(config =>
     config.Connection(builder.Configuration.GetConnectionString("Database")!);
     config.Schema.For<ShoppingCart>().Identity(x => x.UserName);
 }).UseLightweightSessions();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
+
 var app = builder.Build();
 
 // configure HTTP request pipeline
 app.MapCarter();
 app.UseExceptionHandler(options => { });
+app.UseHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 app.Run();
